f22c6446025bf23f417533131060a02074e87774
Ramitha-Silva <104267362+Ramitha-Silva@users.noreply.github.com>
2023-09-11 13:57:38 +0530

    Update index.js
    server port change

--- index.js
+++ index.js
@@ -19,28 +19,28 @@
 
 //ssl code
 
-var fs = require('fs');
+/* var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var privateKey  = fs.readFileSync('sslcert/newpkey.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/www_stargarments_lk.crt', 'utf8');
+var certificate = fs.readFileSync('sslcert/www_stargarments_lk.crt', 'utf8'); */
 
-var credentials = {key: privateKey, cert: certificate};
+//var credentials = {key: privateKey, cert: certificate};
 //const express = require('express');
 //const app = express();
 
 // your express configuration here
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+//var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen("8080",()=>{
-    console.log('HTTP Server Started at port:'+"8080");
+httpServer.listen("5050",()=>{
+    console.log('HTTP Server Started at port:'+"5050");
 });
-httpsServer.listen("8443",()=>{
+/* httpsServer.listen("8443",()=>{
     console.log('HTTPS Server Started at port:'+"8443");
 });
-
+ */
 
 //ssl code
 
