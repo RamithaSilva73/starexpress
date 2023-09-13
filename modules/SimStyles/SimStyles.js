79e476a5f9f76433e17d22c5b4ddb7d5139baf65
Amila79 <amilas@gmail.com>
2023-09-13 09:32:32 +0530

    Update simstyles

--- modules/SimStyles/SimStyles.js
+++ modules/SimStyles/SimStyles.js
@@ -4,21 +4,14 @@ const customError = require('./../../utilities/CustomError')
 const asyncErrorHandler = require('./../../utilities/asyncErrorHandler');
 
 exports.addstyle= asyncErrorHandler(async(req, res,next) => {
-  const outVal = await validation.SimStylesValidation(req,res)      
-  if(outVal==0) {
-    const err = new customError('Oracle Style Code Not Exists In Star Database',406)
-    return next(err) 
-  }
+   const outVal = await validation.SimStylesValidation(req,res)  
 
-
-  
-  /* if(outVal==1) {
-    const err = new customError('1st Test',406)
-    return next(err) 
-  } */
-  
-  const output = await simstylesMssql.addstyle(req.body);
-  res.status(200).send(output);
+   if(outVal.trim()!=='OK') {
+     const err = new customError(outVal,406)
+     return next(err) 
+   }
+   const output = await simstylesMssql.addstyle(req.body);
+   res.status(200).send(output);
 })
 
 
