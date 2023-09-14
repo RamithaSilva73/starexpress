cc9a20f691d452fb58090fef42122786183656bc
Ramitha-Silva <104267362+Ramitha-Silva@users.noreply.github.com>
2023-09-14 19:25:53 +0530

    index updated

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
 
 
-// non ssl
-
 
  const app = require('./app');
  const http =require('http')
 
 
-//non ssl
-
-
-
-
-//ssl code
-
-/* var fs = require('fs');
-
-
-
-//var credentials = {key: privateKey, cert: certificate};
-//const express = require('express');
-//const app = express();
-
-
-// your express configuration here
 
 var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
+
 
 
 
@@ -35,7 +16,4 @@ var httpServer = http.createServer(app);
 httpServer.listen("5050",()=>{
     console.log('HTTP Server Started at port:'+"5050");
 });
-
-
-//ssl code
 
