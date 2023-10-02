8ea7c8080d41ffb6c7165552e3078e99d5651338
Ramitha-Silva <104267362+Ramitha-Silva@users.noreply.github.com>
2023-10-02 11:37:24 +0530

    exceptions api

--- modules/Exceptions/exception.mssql.js
+++ modules/Exceptions/exception.mssql.js
@@ -45,26 +45,37 @@ class exceptionMSSql {
 
 
 
- async updatestyle(style) {
-   const conn = await mssqlcon.getConnection();
-   const res = await conn.request()
-   .input("stylenumber", style.stylenumber)
-   .input("stylename", style.stylename)
-   .input("account", style.account)
-   .input("orderqty",style.orderqty)
-   .input("merchant", style.merchant)
-   .input("product", style.producttype)
-   .execute("updatestyle");
-   return res;
+ async updatstarexception(exception) {
+  const conn = await mssqlcon.getConnection();
+  const res = await conn.request()
+  .input("cService", exception.Service)
+  .input("cError", exception.Error)
+  .input("cSeverity", exception.Severity)
+  .input("cAction", exception.Action)
+  .input("cDescription", exception.Description)
+  .input("cUser", exception.User)
+  .input("Clear", exception.Clear)
+  .execute("updatestarerrorlog");
+  return res;
  }
 
 
- async deletestyle(id) {
-   const conn = await mssqlcon.getConnection();
-   const res = await conn.request()
-   .input("style_id", id)
-   .execute("deletestyle");
-   return res;
- }
+ async updateoracleexception(exception) {
+  const conn = await mssqlcon.getConnection();
+  const result =  await conn.request()
+  .input("Method",exception.method)
+  .input("ApiEndPoint",exception.fullurl)
+  .input("JsnBody",exception.jsnbody)
+  .input("Status",exception.statuscode)
+  .input("Message",exception.msg)
+  .input("StackTrace",exception.stack)
+  .input("Json",exception.jsnfile)
+  .input("Action",exception.Action)
+  .input("Clear",exception.Clear)
+  .execute("addError") 
+  return result;
+}
+
+
 }
 module.exports = new exceptionMSSql();
