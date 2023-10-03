dc2a8742e937e5f48a2a12849274abf71d05bfd9
Ramitha-Silva <104267362+Ramitha-Silva@users.noreply.github.com>
2023-10-03 23:23:12 +0530

    changes lot master and isw summary

--- modules/LotMasterData/LotMasterDataController.js
+++ modules/LotMasterData/LotMasterDataController.js
@@ -6,6 +6,7 @@ class MasterlotDataController {
     constructor(app) {
        
         router.post('/',LotMasterData.addLotMasterData);
+        router.get('/',LotMasterData.getlotmasterHistory);
         app.use('/api/v2/masterlotdata', router);
 
     }
